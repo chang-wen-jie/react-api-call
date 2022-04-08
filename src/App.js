4d9c05a7fb7010e52c8d9e2047d5bf184ee783f3
Chang Wen Jie <changwenjie1999@gmail.com>
2022-04-08 13:53:19 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,37 +1,59 @@
 import React, { useState, useEffect } from "react";
+import { Link, Outlet } from "react-router-dom";
 import "./App.css";
 
-function App() {
-	const [pokemonData, setPokemonData] = useState([]);
-  const [value, setValue] = useState([])
-  
+// function App() {
+// 	const [pokemonData, setPokemonData] = useState([]);
+//   // const [value = 2, setValue] = useState([])
+//   const value = 432
 
-  useState(() => {
-  	fetch("https://pokemonapi.net/api/v2/pokemon/")
-    .then((response) => response.json())
-    .then((data) => {
-    	console.log(data);
-      setPokemonData(data.results);
-    })
-    .catch((error) => {
-    	console.log(error);
-    });
-  }, [value]);
+//   useState(() => {
+//   	fetch("https://pokeapi.co/api/v2/pokemon/" + value)
+//     .then((response) => response.json())
+//     .then((data) => {
+//     	console.log("wat is pokedata", data);
+//       setPokemonData(data);
+//     })
+//     .catch((error) => {
+//     	console.log("Foutmelding:", error);
+//     });
+//   }, [value]);
   
-  const handleChange = (value) => {
-    setValue(value ? value : 25);
-  }
+//   const handleChange = (value) => {
+//     // setValue(value ? value : 25);
+//     console.log("wat is mijn value", value);
+//   }
   
+//   return (
+//   	<div className="app">
+//       <div className="pokemonSearch">
+//         <input
+//           type="number"
+//           value={value}
+//           onChange={(e) => handleChange(e.target.value)}
+//         />
+//       </div>
+//       <div className="pokemonDisplay">
+//         { pokemonData && <img style={{height: 100}} src={pokemonData?.sprites?.front_default} />}
+//       </div>
+//     </div>
+//   )
+// }
+
+export default function App() {
   return (
-  	<div className="app">
-    	<input
-      	type="number"
-        value={value}
-        onChange={(e) => handleChange(e.target.value)}
-      />
-      { pokemonData && <img src={pokemonData?.sprites?.front_default} />}
+    <div>
+      <h1>Bookkeeper!</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+      <Outlet />
     </div>
-  )
+  );
 }
-
-export default App;
